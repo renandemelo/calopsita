b83065ca47cccaf0825828d21fe5e3f2f533f34f
José David F. Curado <davidcurado@mercurio.eclipse.ime.usp.br>
2010-03-23 15:50:33 -0300

    Made a unit test for LifeCycledCard

---
+++ src/test/java/br/com/caelum/calopsita/plugins/lifeCycle/LifeCycledCardTest.java
@@ -0,0 +1,57 @@
+package br.com.caelum.calopsita.plugins.lifeCycle;
+
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.calopsita.model.Card;
+import br.com.caelum.calopsita.persistence.dao.AbstractDaoTest;
+import br.com.caelum.calopsita.plugins.lifeCycle.LifeCycledCard;
+
+
+public class LifeCycledCardTest extends AbstractDaoTest{
+	
+	private Card card;
+	private LifeCycledCard lifeCicledCard;
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		this.card = new Card();
+		session.save(card);		
+		lifeCicledCard = new LifeCycledCard();
+		lifeCicledCard.setCard(card);		
+		session.flush();		
+	}
+	
+	@Test
+	public void saveALifeCicledCard() throws Exception {
+		session.save(lifeCicledCard);		
+		Long id = lifeCicledCard.getId();		
+		LifeCycledCard savedCicledCard = (LifeCycledCard) session.load(LifeCycledCard.class, id);
+		Card savedCard = savedCicledCard.getCard();
+		Assert.assertEquals(card, savedCard);
+	}
+
+	
+	@Test
+	public void saveACardWithCreationDate() throws Exception {
+		LocalDate date = new LocalDate();
+		lifeCicledCard.setCreationDate(date);		
+		session.save(lifeCicledCard);
+		Long id = lifeCicledCard.getId();
+		LifeCycledCard savedCicledCard = (LifeCycledCard) session.load(LifeCycledCard.class, id);
+		Assert.assertEquals(date, savedCicledCard.getCreationDate());
+	}
+	
+	@Test
+	public void saveACardWithAFinishDate() throws Exception {
+		LocalDate date = new LocalDate();
+		lifeCicledCard.setFinishDate(date);		
+		session.save(lifeCicledCard);
+		Long id = lifeCicledCard.getId();		
+		LifeCycledCard savedCicledCard = (LifeCycledCard) session.load(LifeCycledCard.class, id);
+		Assert.assertEquals(date, savedCicledCard.getFinishDate());
+	}
+}
---
+++ src/test/java/br/com/caelum/calopsita/plugins/lifeCycle/LifeCycledEnumGagdetTest.java
@@ -0,0 +1,41 @@
+package br.com.caelum.calopsita.plugins.lifeCycle;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import br.com.caelum.calopsita.model.Card;
+import br.com.caelum.calopsita.model.Gadgets;
+import br.com.caelum.calopsita.persistence.dao.AbstractDaoTest;
+import br.com.caelum.calopsita.persistence.dao.CardDao;
+import br.com.caelum.calopsita.plugins.PluginResultTransformer;
+import br.com.caelum.calopsita.plugins.Transformer;
+import br.com.caelum.calopsita.plugins.prioritization.OrderByPriorityTransformer;
+
+public class LifeCycledEnumGagdetTest extends AbstractDaoTest{
+
+	private Card card;	
+	
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		this.card = new Card(new CardDao(session, new PluginResultTransformer(session, Arrays.<Transformer>asList(new OrderByPriorityTransformer()))));
+		this.card.save();
+	}
+	
+	@Test @Ignore
+	public void testCreationOfLifeCycledCard() throws Exception {
+		List<Gadgets> gadgets = Arrays.asList(Gadgets.LIFE_CYCLE);		
+		card.addGadgets(gadgets);
+		session.flush();
+		LifeCycledCard lifeCycledCard = (LifeCycledCard) card.getGadgets().get(0);
+		Assert.assertNotNull(lifeCycledCard.getCreationDate());
+		Assert.assertEquals(new LocalDate(), lifeCycledCard.getCreationDate());
+	}
+	
+}
