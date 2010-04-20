388d9354e4220772301ad29e63de8f45890aaa42
Santiago <sbenavid@ime.usp.br>
2010-04-20 13:47:57 -0300

    Integration test for assignable cards

---
+++ src/test/integration/br/com/caelum/calopsita/integration/stories/CardOwnerStory.java
@@ -0,0 +1,47 @@
+package br.com.caelum.calopsita.integration.stories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.caelum.calopsita.integration.stories.common.DefaultStory;
+
+
+/**
+ * <b>In order to</b> avoid rework <br>
+ * <b>As a</b> Collaborator <br>
+ * <b>I want to </b> notify that I am working on that card <br>
+ * @author santiago
+ *
+ */
+public class CardOwnerStory extends DefaultStory {
+
+	@Test
+	public void addingAnAssignableCard() {
+		given.thereIsAnUserNamed("adriano").and()
+			.thereIsAProjectNamed("Marriage").ownedBy("adriano")
+			.withAnIterationWhichGoalIs("Postpone").and()
+			.iAmLoggedInAs("adriano");
+		when.iOpenProjectPageOf("Marriage").and()
+			.iOpenCardsPage().and()
+			.iAddTheCard("schedule date")
+				.assignableCard().planningCard()
+				.withDescription("we need a date for marriage").and()
+			.iOpenCardsPage().and()
+			.iOpenAllCardsPage();
+		then.theCard("schedule date").hasOwnerGadget();
+	}
+	
+//	@Test
+//	public void addingOwnerToAssignableCard() throws Exception {
+//		given.thereIsAnUserNamed("adriano").and()
+//			.thereIsAProjectNamed("Marriage").ownedBy("adriano")
+//			.withACurrentIterationWhichGoalIs("Postpone").and()
+//			.withACardNamed("schedule date").planningCard().assignable().and()
+//			.iAmLoggedInAs("adriano");
+//		when.iOpenProjectPageOf("Marriage").and()
+//			.iOpenThePageOfCurrentIteration().and()
+//			.iClickOn("Be an Owner now!");
+//		then.theCard("schedule date").isOwnedBy("adriano");
+//	}
+}
